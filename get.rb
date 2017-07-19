1a3717b7f8d61cc126c5153ae3371a61a7baf68d
Jason Toy <jasontoy@gmail.com>
2017-07-19 02:14:59 -0700

    cleanups

--- get.rb
+++ get.rb
@@ -8,19 +8,15 @@ puts r.items.size
 puts r.to_h.keys
 total =r['total_count']
 counter = 0
-#while counter <  total
-puts r[:next]
 next_url =  client.last_response.rels[:next]
 items = r.items
 while items
   items.each do |item|
-    puts 
-  url = item['clone_url']
-  puts "cloning #{url}"
-  `cd #{searchterm} && git clone #{url}`
-  counter +=1
- end
- puts counter
+    url = item['clone_url']
+    puts "cloning #{url}"
+    `cd #{searchterm} && git clone #{url}`
+    counter +=1
+  end
   if client.last_response.rels[:next]
     items = client.last_response.rels[:next].get.data.items
   else
